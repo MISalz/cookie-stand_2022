2d6eb49971e6ba3131d3df2123f15ef77bcc8336
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-02-15 10:37:48 -0800

    Linked files, CSS files, Added assignment requirements in JS.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// needs to calculate the number of cookies each location must make every day so that it can manage its supplies inventory and baking schedule. The number of cookies to make depends on the hours of operation (6:00 AM to 8:00 PM for all locations) and a few factors unique to each location:
+
+// The minimum number of customers per hour.
+// The maximum number of customers per hour.
+// The average number of cookies purchased per customer
+
+// JS object literals for each shop location that outputs the following to the sales.html file:
+
+// Stores the min/max hourly customers, and the average cookies per customer, in object properties
+// Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
+// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+// Store the results for each location in a separate array… perhaps as a property of the object representing that location
+// Display the values of each array as unordered lists in the browser
+// Calculating the sum of these hourly totals; your output for each location should look like this:
+
+// Make sure to make each location is its own JavaScript object.
+
+let sales = document.getElementById('sales');
