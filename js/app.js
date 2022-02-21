44be503968165da116bc17b02530d6874dd41779
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-02-20 21:31:52 -0800

    Objects and DOM elements

--- js/app.js
+++ js/app.js
@@ -1,22 +1,52 @@
 'use strict';
-
-// needs to calculate the number of cookies each location must make every day so that it can manage its supplies inventory and baking schedule. The number of cookies to make depends on the hours of operation (6:00 AM to 8:00 PM for all locations) and a few factors unique to each location:
+//in object properties; JS object literals for each shop location
 
 // The minimum number of customers per hour.
 // The maximum number of customers per hour.
 // The average number of cookies purchased per customer
-
-// JS object literals for each shop location that outputs the following to the sales.html file:
-
-// Stores the min/max hourly customers, and the average cookies per customer, in object properties
+// needs to calculate the number of cookies each location must make every day,The number of cookies to make depends on the hours of operation 
+// Calculating the sum of these hourly totals; 
 // Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+
+
 // Store the results for each location in a separate array… perhaps as a property of the object representing that location
+
+// that outputs the following to the sales.html file:
+// Stores the min/max hourly customers
+// average cookies per customer, 
+
+// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+
 // Display the values of each array as unordered lists in the browser
-// Calculating the sum of these hourly totals; your output for each location should look like this:
-
-// Make sure to make each location is its own JavaScript object.
 
 
+// let stores = ['Seattle', 'Tokyo','Dubai','Paris','Lima']
+let storeHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
 
-let sales = document.getElementById('sales');
+
+ let sales = document.getElementById('SalesByLocation');
+
+let seattle = {
+  location: 'Seattle',
+  hours: storeHours,
+  minCust:23,
+  maxCust:65,
+  avgCookieSale:6.3,
+  avgCustPerHour:function(){
+    for(let i=0, i < storeHours.length; i++){
+      storeHours.length(custPerHour);
+
+    }
+  },
+  cookiesPerHour:[],
+  totalCookiesPerDay:[],
+
+}
+
+function custPerHour(min,max){
+return Math.floor(Math.random()*(min-max+1)+min)
+}
+
+seattle.render= function(){
+  let 
+}
